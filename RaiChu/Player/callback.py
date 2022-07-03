05111c3c93371b4cd4b4f3d724030c1d45e7e7fb
CANDYOP01 <108625949+CANDYOP01@users.noreply.github.com>
2022-07-03 22:47:14 +0530

    Update callback.py

--- RaiChu/Player/callback.py
+++ RaiChu/Player/callback.py
@@ -92,7 +92,7 @@ async def cbbasic(_, query: CallbackQuery):
 
 🤷 » /skip - To Skip current song
 
-🤷 » /repo - To get the repo of RaiChu-Music
+🤷 » /repo - To get the repo of candy
 
 🙋 » /end - To end play song in vc.""",
         reply_markup=InlineKeyboardMarkup(
